7fa7632f035f9a45d6e41dc8c601455c9a1b72ef
elliasc04 <guoyethan@gmail.com>
2023-08-18 13:16:46 -0500

    separation of view functions

--- scraperbackend/scraper/urls.py
+++ scraperbackend/scraper/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('scrape/<path:url>/', views.scrape_data, name='scrape_data'),
+    path('getlivebusyness/<path:url>/', views.get_live_busyness, name='get live busyness'),
+    path('getpopulartimes/<path:url>/', views.get_popular_times, name='get popular times'),
 ]
--- scraperbackend/scraper/views.py
+++ scraperbackend/scraper/views.py
@@ -4,32 +4,38 @@ from .models import Scraper
 from .main import MapsPage  # Import your scraping logic
 import json
 
+def scrape_data(url):
+    maps_page = MapsPage(url)
+
+    live_busyness_json = maps_page.retlivebusyness()
+    populartimes_json = maps_page.retpopulartimes()
+
+    live_busyness_data = json.loads(live_busyness_json)
+    populartimes_data = json.loads(populartimes_json)
+
+    scraper, created = Scraper.objects.update_or_create(
+        url=url,
+        defaults={'livebusyness': live_busyness_data, 'populartimes': populartimes_data}
+    )
+
+    return live_busyness_data, populartimes_data
+
 @csrf_exempt
-def scrape_data(request, url):
+def get_live_busyness(request, url):
     if request.method == 'GET':
         try:
-            maps_page = MapsPage(url)
-            
-            # Call the methods to get JSON data
-            live_busyness_json = maps_page.retlivebusyness()
-            populartimes_json = maps_page.retpopulartimes()
-
-            # Parse JSON data into Python dictionaries or lists
-            live_busyness_data = json.loads(live_busyness_json)
-            populartimes_data = json.loads(populartimes_json)
-
-            # Create or update the Scraper instance
-            scraper, created = Scraper.objects.update_or_create(
-                url=url,
-                defaults={'livebusyness': live_busyness_data, 'populartimes': populartimes_data}
-            )
-
-            # Return a success response with the scraped data
-            return JsonResponse({'success': 'Data saved successfully',
-                                 'livebusyness': live_busyness_data,
-                                 'populartimes': populartimes_data
-                                 })
+            live_busyness_data, _ = scrape_data(url)
+            return JsonResponse({'success': 'Live busyness data saved successfully', 'livebusyness': live_busyness_data})
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
 
+@csrf_exempt
+def get_popular_times(request, url):
+    if request.method == 'GET':
+        try:
+            _, populartimes_data = scrape_data(url)
+            return JsonResponse({'success': 'Popular times data saved successfully', 'populartimes': populartimes_data})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
     return JsonResponse({'error': 'Invalid request method'}, status=400)
