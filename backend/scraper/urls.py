e277fe4362ba6575f09623b3c51069d94bc5ade7
elliasc04 <guoyethan@gmail.com>
2023-09-24 21:59:10 -0500

    Update urls.py

--- backend/scraper/urls.py
+++ backend/scraper/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getlivebusyness/<path:url>/', views.get_live_busyness, name='get live busyness'),
+    # path('getlivebusyness/<path:url>/', views.get_live_busyness, name='get live busyness'),
     path('getpopulartimes/<path:url>/', views.get_popular_times, name='get popular times'),
     path('', views.index),
 ]
