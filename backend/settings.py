3ccfa13ec0d37dc5eada38502989d9e6522dfba4
elliasc04 <guoyethan@gmail.com>
2023-09-26 23:17:06 -0500

    fix: vercel.json

--- backend/settings.py
+++ backend/settings.py
@@ -80,10 +80,6 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
 }
 
 
