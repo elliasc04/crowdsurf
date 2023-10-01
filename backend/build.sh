4c8529eba63e22d0ee5b526f9f12d630ca7c4369
elliasc04 <guoyethan@gmail.com>
2023-10-01 18:49:28 -0500

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,7 @@
 
 # Build the project
 echo "Building the project..."
+sudo apt install pkg-config
 python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
