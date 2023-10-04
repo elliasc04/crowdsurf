8799917c8a6a0c26b83112416ad13f9ddfc89132
elliasc04 <guoyethan@gmail.com>
2023-10-03 23:32:32 -0500

    downgrade django

--- backend/scraper/scraper/settings.py
+++ backend/scraper/scraper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=9*#*9%kz4s7+b^rykgbneblu*feef4phywzzvk0s_$13d^gyi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
